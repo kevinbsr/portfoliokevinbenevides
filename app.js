07cf50f2f6b60792d37f53e16c950fb86c91bddb
Kevin Benevides <61016019+kevinbsr@users.noreply.github.com>
2023-02-26 19:27:15 -0300

    Form update

--- app.js
+++ app.js
@@ -17,10 +17,10 @@ function clickHandler(e) {
 }
 
 // Form submission
-const form = document.querySelector('form');
+/* const form = document.querySelector('form');
 const nameInput = document.getElementById('name');
 const emailInput = document.getElementById('email');
-const messageInput = document.getElementById('message');
+const messageInput = document.getElementById('message'); */
 
 /* form.addEventListener('submit', (e) => {
 	e.preventDefault();
